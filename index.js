31202876ee70eb9875efa41d7d3596a5aa9c3fef
vassaneer <pearitono@gmail.com>
2020-04-12 16:18:20 +0700

    make it better

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 
 // use cor
-app.use(cors());
+// app.use(cors());
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'https://jonghor-ba5d2.web.app')
     res.header('Access-Control-Allow-Methods','POST, GET, PUT, PATCH, DELETE, OPTIONS')
