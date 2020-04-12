eb13a6696be4608678cb95d9de6962032ad06f8a
vassaneer <pearitono@gmail.com>
2020-04-12 16:31:48 +0700

    make it better

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.use(bodyParser.urlencoded({extended:true}))
 // app.use(cors());
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'https://jonghor-ba5d2.web.app')
+    res.header('Access-Control-Allow-Credentials', true)
     res.header('Access-Control-Allow-Methods','POST, GET, PUT, PATCH, DELETE, OPTIONS')
     res.header('Access-Control-Allow-Headers','Content-Type, Option, Authorization')
     return next()
